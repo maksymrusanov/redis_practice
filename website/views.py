6e4dca4257af7faf12a7821b9ca3c152e0429999
maksym <1maksymrusanov1@gmail.com>
2025-10-21 15:24:14 +0000

    create simple logic for app

--- website/views.py
+++ website/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 
 def main(request):
-    return HttpResponse('<h1>main page<h1>')
+    list = [i for i in range(10)]
+
+    return render(request, template_name='main.html', context={'list': list})
+
+
+def qwe(request):
+    list = [i for i in range(10, 20)]
+
+    return render(request, template_name='sec.html', context={'list': list})
